4b05f7c8797f93607830d968aaf430212b3b67ff
dmeza38 <dmeza38@github.com>
2022-03-18 14:46:53 -0700

    Changed to make function call

--- dmesa.h
+++ dmesa.h
@@ -4,12 +4,16 @@
 //Author: David Mesa
 //This is my header file to show case my contribtion to the project
 
+// created a create class for credit screen 
 class Credits
 {
     public:
-        void showPage();
+        void showPage(int xres, int yres);
         
 };
+
+//extern void ggprint8b(Rect *r, int advance, int cref, const char *fmt, ...);
+
 
 
 #endif
