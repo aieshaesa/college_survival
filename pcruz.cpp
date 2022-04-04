8f60b5b1e00b375d3a20df4427c37d6920502710
Patrick Cruz <83104613+Cruz0421@users.noreply.github.com>
2022-04-04 10:36:41 -0700

    Update pcruz.cpp

--- pcruz.cpp
+++ pcruz.cpp
@@ -5,6 +5,26 @@
 
 //This calls the credits page.
 #include <stdio.h>
-#include "dmesa.h"
+#include <std>
 
-Credits credits;
+
+void myodin() 
+{
+    string temp;
+    cout<<"enter yes for odin username: ";
+    cin >> temp;
+    
+    while(temp != "yes") {
+        cout << "enter yes for odin username: ";
+        cin >> temp;
+    }
+    cout << "pcruz";
+    
+}
+int main()
+{
+    myodin();
+    return 0;
+}
+
+
