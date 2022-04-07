5bd1b905f758e94bec815e7298274b49f1157b59
Ranbir Grewal <rgrewal@odin.cs.csubak.edu>
2022-04-07 11:39:34 -0700

    midterm changes

--- rgrewal.cpp
+++ rgrewal.cpp
@@ -1,7 +1,29 @@
 //Ranbir Grewal
+// I am in charge of the controls of this game
+#include <iostream>
+typedef struct t_rect 
+{
+    int left;
+    int top;
+    int right;
+    int bot;
+    int width;
+    int height;
+    int center;
+    int centerx;
+    int centery;
+} Rect;
 
-#include <stdio.h>
-#include "rgrewal.h"
-#include <GL/glx.h>
 
+extern void ggprint8b(Rect *r, int advance, int cref, const char *fmt, ...);
+Rect r;
+void rgrewal(){
+    
+    unsigned int c = 0x00000000;
+    ggprint8b(&r, 16, c, "David Mesa");
+    ggprint8b(&r, 16,c, "Patrick Cruz");
+    ggprint8b(&r, 16,c, "Aiesha Esa");
+    ggprint8b(&r, 16,c, "Ranbir Grewal");
+
+}
 
