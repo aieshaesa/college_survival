275f83488983ad96b8d83620f8b26c5b2aa8c050
dmeza38 <dmeza38@github.com>
2022-04-18 14:16:51 -0700

    Added a new function

--- dmesa.cpp
+++ dmesa.cpp
@@ -9,7 +9,7 @@
 #include <GL/glx.h>
 #include <iostream>
 #include <math.h>
-#include <string.h>
+#include <string>
 using namespace std;
 Dmesa dmesa;
 
@@ -44,7 +44,7 @@ extern void ggprint06(Rect *r, int advance, int cref, const char *fmt, ...);
 Credits credits; 
 
 // add showpage function for my credit screen 
-void Credits :: showPage(int xres, int yres)
+void Credits :: showPage(int xres, int yres, unsigned int texid, int wd, int h)
 {
 
 
@@ -98,66 +98,31 @@ void Credits :: showPage(int xres, int yres)
     r.center =0;
 
     ggprint06(&r, 16, b, "Ranbir Grewal");
-}
-class Image {
-public:
-    int width, height;
-    unsigned char *data;
-    ~Image() { delete [] data; }
-    Image(const char *fname) {
-        if (fname[0] == '\0')
-            return;
-        //printf("fname **%s**\n", fname);
-        int ppmFlag = 0;
-        char name[40];
-        strcpy(name, fname);
-        int slen = strlen(name);
-        char ppmname[80];
-        if (strncmp(name+(slen-4), ".ppm", 4) == 0)
-            ppmFlag = 1;
-        if (ppmFlag) {
-            strcpy(ppmname, name);
-        } else {
-            name[slen-4] = '\0';
-            //printf("name **%s**\n", name);
-            sprintf(ppmname,"%s.ppm", name);
-            //printf("ppmname **%s**\n", ppmname);
-            char ts[100];
-            //system("convert eball.jpg eball.ppm");
-            sprintf(ts, "convert %s %s", fname, ppmname);
-            system(ts);
-        }
-        //sprintf(ts, "%s", name);
-        FILE *fpi = fopen(ppmname, "r");
-        if (fpi) {
-            char line[200];
-            fgets(line, 200, fpi);
-            fgets(line, 200, fpi);
-            //skip comments and blank lines
-            while (line[0] == '#' || strlen(line) < 2)
-                fgets(line, 200, fpi);
-            sscanf(line, "%i %i", &width, &height);
-            fgets(line, 200, fpi);
-            //get pixel data
-            int n = width * height * 3;
-            data = new unsigned char[n];
-            for (int i=0; i<n; i++)
-                data[i] = fgetc(fpi);
-            fclose(fpi);
-        } else {
-            printf("ERROR opening image: %s\n",ppmname);
-            exit(0);
-        }
-        if (!ppmFlag)
-            unlink(ppmname);
-    }
-};
 
-// Aiesha changed Bigfoot image to metalslug, and forest image to campus
-/*Image img[4] = {
-"./images/metalslug.png",
-"./images/campus.png",
-"./images/forestTrans.png",
-"./images/umbrella.png" };
-*/
+    glPushMatrix();
+    glTranslatef(xres/2,yres/2,0);
+    float w = 50.0;
+    glColor3ub(255,255,255);
+    glBindTexture(GL_TEXTURE_2D,texid);
+    glBegin(GL_QUADS);
+        glTexCoord2f(0.0f,1.0f); glVertex2f(-w,-w);
+        glTexCoord2f(0.0f,0.0f); glVertex2f(-w, w);
+        glTexCoord2f(1.0f,0.0f); glVertex2f( w, w);
+        glTexCoord2f(1.0f,1.0f); glVertex2f( w,-w);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glPopMatrix();
+
+}
+Texture tex;
+void Texture :: maketext(unsigned int texid, int width, int height, const void* img)
+{   
+    glBindTexture(GL_TEXTURE_2D, texid);
+     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height , 0,
+    GL_RGB, GL_UNSIGNED_BYTE, img);
+    glBindTexture(GL_TEXTURE_2D,0);
+}
+
 
