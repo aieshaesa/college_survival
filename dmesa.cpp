1a0ff53a4034a56e50d84b1e83602bd336bde6f2
dmeza38 <dmeza38@github.com>
2022-04-17 23:21:28 -0700

    made changes to credit page

--- dmesa.cpp
+++ dmesa.cpp
@@ -3,10 +3,13 @@
 // This is showing my ability to code and my contribution to the group
 //
 //
+#include <unistd.h>
 #include <stdio.h>
 #include "dmesa.h"
 #include <GL/glx.h>
 #include <iostream>
+#include <math.h>
+#include <string.h>
 using namespace std;
 Dmesa dmesa;
 
@@ -41,32 +44,120 @@ extern void ggprint06(Rect *r, int advance, int cref, const char *fmt, ...);
 Credits credits; 
 
 // add showpage function for my credit screen 
-//void Credits :: showPaget(int xres, int yres)
-void Credits :: showPage(int yres)
+void Credits :: showPage(int xres, int yres)
 {
-    Rect r;
 
- //   glBindTexture(GL_TEXTURE_2D, g.forestTexture);
-  //  glBegin(GL_QUADS);
-  //  glTexCoord2f(0.0f, 1.0f); glVertex2i(0, 0);
-  //  glTexCoord2f(0.0f, 0.0f); glVertex2i(0, g.yres);
-  //  glTexCoord2f(1.0f, 0.0f); glVertex2i(g.xres, g.yres);
-  //  glTexCoord2f(1.0f, 1.0f); glVertex2i(g.xres, 0);
-  //  glEnd();
 
-  // chooses color to  and displays name for credit space
-  glClearColor(1.0,1.0,1.0,1.0);
+  Rect r;
+  glClearColor(1.0f,1.0f,0.0f,0.0f);
   glClear(GL_COLOR_BUFFER_BIT);
-  unsigned int c = 0x000000fff;
-  r.bot = yres - 20;
-  r.left = 10;
-  r.center = 0;
-//  ggprint8b(
-//  glTranslate(xres/2,yres/2,0);
-//  glEnd();
-    ggprint8b(&r, 16, c, "David Mesa");
-    ggprint8b(&r, 16, c, "Patrick Cruz");
-    ggprint8b(&r, 16, c, "Aiesha Esa");
-    ggprint8b(&r, 16, c, "Ranbir Grewal");
+  glColor3ub(0,0,255);
+  glPushMatrix();
 
+   static float rot = 0.0;
+   static float angle = 0.0;
+   glTranslatef(xres/2,yres/2,0);
+   glRotatef(rot,0.0f,0.0f,1.0f);
+   glTranslatef(-xres/2, -yres/2,0);
+   rot = sin(angle)* 20;
+   angle +=1.5;
+
+   glBegin(GL_TRIANGLES);
+     glVertex2f(20.0,20.0);
+     glVertex2f(xres/2.0,yres-20);
+     glVertex2f(xres-20,20.0);
+   glEnd();
+   glPopMatrix(); 
+   
+    // chooses color to  and displays name for credit space
+//  glClearColor(0.0f,0.0f,0.0f,0.0f);
+// glClear(GL_COLOR_BUFFER_BIT);
+ unsigned int c = 0x00ffff;
+    r.bot = yres - 20;
+    r.left = 10;
+    r.center =0;
+    ggprint06(&r, 16, c, "David Mesa");
+
+    unsigned int d = 0xaa0000;
+    r.bot = yres - 20;
+    r.left = 100;
+    r.center =0;
+
+    ggprint06(&r, 16, d, "Patrick Cruz");
+
+    unsigned int a = 0x00ff00;
+    r.bot = yres - 20;
+    r.left = 200;
+    r.center =0;
+
+    ggprint06(&r, 16, a, "Aiesha Esa");
+
+    unsigned int b = 0xaa0000;
+    r.bot = yres - 20;
+    r.left = 300;
+    r.center =0;
+
+    ggprint06(&r, 16, b, "Ranbir Grewal");
 }
+class Image {
+public:
+    int width, height;
+    unsigned char *data;
+    ~Image() { delete [] data; }
+    Image(const char *fname) {
+        if (fname[0] == '\0')
+            return;
+        //printf("fname **%s**\n", fname);
+        int ppmFlag = 0;
+        char name[40];
+        strcpy(name, fname);
+        int slen = strlen(name);
+        char ppmname[80];
+        if (strncmp(name+(slen-4), ".ppm", 4) == 0)
+            ppmFlag = 1;
+        if (ppmFlag) {
+            strcpy(ppmname, name);
+        } else {
+            name[slen-4] = '\0';
+            //printf("name **%s**\n", name);
+            sprintf(ppmname,"%s.ppm", name);
+            //printf("ppmname **%s**\n", ppmname);
+            char ts[100];
+            //system("convert eball.jpg eball.ppm");
+            sprintf(ts, "convert %s %s", fname, ppmname);
+            system(ts);
+        }
+        //sprintf(ts, "%s", name);
+        FILE *fpi = fopen(ppmname, "r");
+        if (fpi) {
+            char line[200];
+            fgets(line, 200, fpi);
+            fgets(line, 200, fpi);
+            //skip comments and blank lines
+            while (line[0] == '#' || strlen(line) < 2)
+                fgets(line, 200, fpi);
+            sscanf(line, "%i %i", &width, &height);
+            fgets(line, 200, fpi);
+            //get pixel data
+            int n = width * height * 3;
+            data = new unsigned char[n];
+            for (int i=0; i<n; i++)
+                data[i] = fgetc(fpi);
+            fclose(fpi);
+        } else {
+            printf("ERROR opening image: %s\n",ppmname);
+            exit(0);
+        }
+        if (!ppmFlag)
+            unlink(ppmname);
+    }
+};
+
+// Aiesha changed Bigfoot image to metalslug, and forest image to campus
+/*Image img[4] = {
+"./images/metalslug.png",
+"./images/campus.png",
+"./images/forestTrans.png",
+"./images/umbrella.png" };
+*/
+
