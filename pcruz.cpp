5c130908e35e7b279ca9a4451c1f7af0923931bc
Patrick Cruz <83104613+Cruz0421@users.noreply.github.com>
2022-05-04 21:45:53 -0700

    Update pcruz.cpp

--- pcruz.cpp
+++ pcruz.cpp
@@ -9,6 +9,28 @@
 #include "dmesa.h"
 
 //using namespace std;
+//Bullet function
+void Bullet :: moveBullet(int nbullets) {
+    
+struct timespec bt;
+	clock_gettime(CLOCK_REALTIME, &bt);
+	int i = 0;
+	while (i < g.nbullets) {
+		Bullet *b = &g.barr[i];
+		//How long has bullet been alive?
+		double ts = timeDiff(&b->time, &bt);
+		if (ts > 2.5) {
+			//time to delete the bullet.
+			memcpy(&g.barr[i], &g.barr[g.nbullets-1],
+				sizeof(Bullet));
+			g.nbullets--;
+			//do not increment i.
+			continue;
+		}
+		//move the bullet
+		b->pos[0] += b->vel[0];
+		b->pos[1] += b->vel[1];
+    }
 
 //Midterm: thursday revisions
 //Test that the value falls within a range.
